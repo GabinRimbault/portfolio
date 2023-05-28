fbba69f042e717f63035bc2586f6c487d6744cc6
GabinRimlbault <gabin.rimbault@icloud.fr>
2023-05-29 01:37:02 +0200

    ajout du responsive via bs

--- src/lib/components/base/Img.js
+++ src/lib/components/base/Img.js
@@ -1,10 +1,10 @@
 export default function Img({src, alt = "alt", classImg, ...props}) {
-
+  const style = classImg !== undefined ? classImg + " img-fluid" : "img-fluid"
   let imgsrc = src !== undefined ? src : "http://via.placeholder.com/300x300";
 
   return (
     <>
-      <img src={imgsrc} alt={alt} className={classImg} {...props}/>
+      <img src={imgsrc} alt={alt} className={style} {...props}/>
     </>
   )
 }
