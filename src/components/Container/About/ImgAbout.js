2ede4e96b5a063ff3734de1b69dd4659253c2dc2
GabinRimlbault <gabin.rimbault@icloud.fr>
2023-05-29 03:01:51 +0200

    photos

--- src/components/Container/About/ImgAbout.js
+++ src/components/Container/About/ImgAbout.js
@@ -1,9 +1,10 @@
 import Img from "../../../lib/components/base/Img";
+import PhotoSrc from "./img_perso.png";
 
 export default function ImgAbout() {
   return (
     <div className="img_about">
-      <Img />
+      <Img src={PhotoSrc} alt="Photos Personnel" />
     </div>
   )
 }
